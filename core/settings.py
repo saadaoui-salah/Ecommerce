22e667e95cc0a544231027d5b9ef0159656f1ff9
saadaoui-salah <salahsaadaoui8@gmail.com>
2022-09-17 10:00:25 -0400

    Add debug condition

--- core/settings.py
+++ core/settings.py
@@ -22,9 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ['ENV'] == 'DEV':
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://my-ecommerce12.herokuapp.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
