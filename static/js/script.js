75fd9a99c7cf8122eb09fed5ede6a5c5c1496290
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-01-27 11:54:14 -0500

    add search functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,21 @@
+console.log("searchBtn")
 const profile = document.getElementById('user-dropdown')
 const menu  = document.getElementById('dropdown-menu')
+const searchBtn = document.getElementById('search-btn')
+const searchInpt = document.getElementById('search-inpt')
+
 profile.addEventListener('click', (e) => {
 	if (menu.className.includes('fixed')){
 		menu.className = menu.className.replace('fixed', 'hidden')
 	} else {
 		menu.className = menu.className.replace('hidden', 'fixed')
 	}
+})
+
+searchBtn.addEventListener('click',() => {
+	console.log(searchInpt.value)
+	if (searchInpt.value != ""){
+
+		window.location.href = `${window.location.href}/q=${JSON.stringify(searchInpt.value)}`
+	}	
 })
