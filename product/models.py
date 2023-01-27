72f22262305c04496d2f767b563d1c8ac3f865b0
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-01-27 13:04:13 -0500

    add category

--- product/models.py
+++ product/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from account.models import User
 
+class Category(models.Model):
+    parent     = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    name       = models.CharField(max_length=30)
+    icon       = models.ImageField(upload_to='categories')
+    created_at = models.DateTimeField(auto_now_add=True)
+
 class Product(models.Model):
+    category   = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     name       = models.CharField(max_length=20)
     details    = models.TextField()
     price      = models.FloatField()
     stock      = models.IntegerField()
     image      = models.ImageField(upload_to="media/products")
     created_at = models.DateTimeField(auto_now_add=True)
-
-
 
 class Order(models.Model):
     user         = models.ForeignKey(User, on_delete=models.CASCADE)
